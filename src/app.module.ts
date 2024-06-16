56c4b576ffac0b098bfd222d72e66183a62a5794
Thanh3102 <thanhduong200602@gmail.com>
2024-06-16 12:42:05 +0700

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { WarehouseModule } from './warehouse/warehouse.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { StatisticModule } from './statistic/statistic.module';
 
 @Module({
-  imports: [AuthModule, WarehouseModule],
+  imports: [AuthModule, WarehouseModule, ProductModule, OrderModule, StatisticModule],
   controllers: [AppController],
   providers: [AppService],
 })
