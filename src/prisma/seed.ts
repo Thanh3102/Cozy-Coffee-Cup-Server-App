29a221943d7243fc5d08375d41d180262f0fc52b
Thanh3102 <thanhduong200602@gmail.com>
2024-06-16 12:40:32 +0700

    Update db schema and db seed

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient} from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
-export enum Role {
+enum Role {
   ADMIN = 'ADMIN',
 }
 
 function randomDateTime() {
-  // Chuyển đổi chuỗi thành dạng ngày
   var start = new Date('2023-01-01T00:00:00');
   var end = new Date('2023-12-31T23:59:59');
 
@@ -36,100 +35,27 @@ function randomString(length) {
   return result;
 }
 
-const units: { name: string; short: string | null }[] = [
-  { name: 'Kilogram', short: 'Kg' },
-  { name: 'Gram', short: 'g' },
-  { name: 'Lít', short: 'L' },
-  { name: 'Millilit', short: 'mL' },
-  { name: 'Hộp', short: null },
-  { name: 'Thùng', short: null },
-];
-
-var suppliers = [
-  {
-    companyName: 'ABC Company',
-    address: '123 Đường ABC, Thành phố X, Quốc gia Y',
-    contactInfo: '+123456789, abc@company.com',
-    mainContact: 'John Smith',
-    productsOrServices: 'Máy móc và thiết bị công nghiệp',
-  },
-  {
-    companyName: 'XYZ Corporation',
-    address: '456 Đường XYZ, Thành phố Z, Quốc gia Y',
-    contactInfo: '+987654321, xyz@corporation.com',
-    mainContact: 'Jane Doe',
-    productsOrServices: 'Vật liệu xây dựng',
-  },
-  {
-    companyName: 'DEF Enterprises',
-    address: '789 Đường DEF, Thành phố A, Quốc gia B',
-    contactInfo: '+246813579, def@enterprises.com',
-    mainContact: 'David Johnson',
-    productsOrServices: 'Dịch vụ kỹ thuật số',
-  },
-  {
-    companyName: 'GHI Industries',
-    address: '1011 Đường GHI, Thành phố C, Quốc gia D',
-    contactInfo: '+135792468, ghi@industries.com',
-    mainContact: 'Emily Brown',
-    productsOrServices: 'Linh kiện điện tử',
-  },
-  {
-    companyName: 'LMN Supplies',
-    address: '1213 Đường LMN, Thành phố E, Quốc gia F',
-    contactInfo: '+369258147, lmn@supplies.com',
-    mainContact: 'Michael White',
-    productsOrServices: 'Vật liệu và thiết bị nông nghiệp',
-  },
-  {
-    companyName: 'PQR Solutions',
-    address: '1415 Đường PQR, Thành phố G, Quốc gia H',
-    contactInfo: '+147258369, pqr@solutions.com',
-    mainContact: 'Sarah Wilson',
-    productsOrServices: 'Phần mềm và giải pháp công nghệ thông tin',
-  },
-  {
-    companyName: 'UVW Enterprises',
-    address: '1617 Đường UVW, Thành phố I, Quốc gia J',
-    contactInfo: '+258369147, uvw@enterprises.com',
-    mainContact: 'Kevin Lee',
-    productsOrServices: 'Dịch vụ tài chính',
-  },
-  {
-    companyName: 'RST Manufacturing',
-    address: '1819 Đường RST, Thành phố K, Quốc gia L',
-    contactInfo: '+369147258, rst@manufacturing.com',
-    mainContact: 'Lisa Taylor',
-    productsOrServices: 'Sản xuất và gia công',
-  },
-  {
-    companyName: 'OPQ Corporation',
-    address: '2021 Đường OPQ, Thành phố M, Quốc gia N',
-    contactInfo: '+123789456, opq@corporation.com',
-    mainContact: 'Robert Clark',
-    productsOrServices: 'Dịch vụ tư vấn',
-  },
-  {
-    companyName: 'STU Logistics',
-    address: '2223 Đường STU, Thành phố O, Quốc gia P',
-    contactInfo: '+456123789, stu@logistics.com',
-    mainContact: 'Jessica Martinez',
-    productsOrServices: 'Dịch vụ vận chuyển và logistics',
-  },
-];
-
 const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.import_Note_Detail.deleteMany();
-  await prisma.import_Note.deleteMany();
+const deleteTableRecord = async (prisma: any) => {
+  await prisma.payment.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  await prisma.importNoteDetail.deleteMany();
+  await prisma.importNote.deleteMany();
   await prisma.provider.deleteMany();
   await prisma.material.deleteMany();
   await prisma.unit.deleteMany();
   await prisma.user.deleteMany();
-  await prisma.$queryRaw`ALTER TABLE material AUTO_INCREMENT = 1`;
-  await prisma.$queryRaw`ALTER TABLE unit AUTO_INCREMENT = 1`;
-  const user = await prisma.user.create({
+  await prisma.statisticsByDay.deleteMany();
+
+  await prisma.$queryRaw`ALTER TABLE materials AUTO_INCREMENT = 1`;
+  await prisma.$queryRaw`ALTER TABLE units AUTO_INCREMENT = 1`;
+  await prisma.$queryRaw`ALTER TABLE statistics_by_day AUTO_INCREMENT = 1`;
+};
+
+const createUser = async (prisma) => {
+  await prisma.user.create({
     data: {
       username: 'admin',
       password: await bcrypt.hash('123456', 10),
@@ -137,6 +63,17 @@ async function main() {
       role: Role.ADMIN,
     },
   });
+};
+
+const createUnit = async (prisma) => {
+  const units = [
+    { name: 'Kilogram', short: 'Kg' },
+    { name: 'Gram', short: 'g' },
+    { name: 'Lít', short: 'L' },
+    { name: 'Millilit', short: 'mL' },
+    { name: 'Hộp', short: null },
+    { name: 'Thùng', short: null },
+  ];
 
   units.map(async (unit) => {
     await prisma.unit.create({
@@ -146,7 +83,82 @@ async function main() {
       },
     });
   });
+};
 
+const createProvider = async (prisma) => {
+  var suppliers = [
+    {
+      companyName: 'ABC Company',
+      address: '123 Đường ABC, Thành phố X, Quốc gia Y',
+      contactInfo: '+123456789, abc@company.com',
+      mainContact: 'John Smith',
+      productsOrServices: 'Máy móc và thiết bị công nghiệp',
+    },
+    {
+      companyName: 'XYZ Corporation',
+      address: '456 Đường XYZ, Thành phố Z, Quốc gia Y',
+      contactInfo: '+987654321, xyz@corporation.com',
+      mainContact: 'Jane Doe',
+      productsOrServices: 'Vật liệu xây dựng',
+    },
+    {
+      companyName: 'DEF Enterprises',
+      address: '789 Đường DEF, Thành phố A, Quốc gia B',
+      contactInfo: '+246813579, def@enterprises.com',
+      mainContact: 'David Johnson',
+      productsOrServices: 'Dịch vụ kỹ thuật số',
+    },
+    {
+      companyName: 'GHI Industries',
+      address: '1011 Đường GHI, Thành phố C, Quốc gia D',
+      contactInfo: '+135792468, ghi@industries.com',
+      mainContact: 'Emily Brown',
+      productsOrServices: 'Linh kiện điện tử',
+    },
+    {
+      companyName: 'LMN Supplies',
+      address: '1213 Đường LMN, Thành phố E, Quốc gia F',
+      contactInfo: '+369258147, lmn@supplies.com',
+      mainContact: 'Michael White',
+      productsOrServices: 'Vật liệu và thiết bị nông nghiệp',
+    },
+    {
+      companyName: 'PQR Solutions',
+      address: '1415 Đường PQR, Thành phố G, Quốc gia H',
+      contactInfo: '+147258369, pqr@solutions.com',
+      mainContact: 'Sarah Wilson',
+      productsOrServices: 'Phần mềm và giải pháp công nghệ thông tin',
+    },
+    {
+      companyName: 'UVW Enterprises',
+      address: '1617 Đường UVW, Thành phố I, Quốc gia J',
+      contactInfo: '+258369147, uvw@enterprises.com',
+      mainContact: 'Kevin Lee',
+      productsOrServices: 'Dịch vụ tài chính',
+    },
+    {
+      companyName: 'RST Manufacturing',
+      address: '1819 Đường RST, Thành phố K, Quốc gia L',
+      contactInfo: '+369147258, rst@manufacturing.com',
+      mainContact: 'Lisa Taylor',
+      productsOrServices: 'Sản xuất và gia công',
+    },
+    {
+      companyName: 'OPQ Corporation',
+      address: '2021 Đường OPQ, Thành phố M, Quốc gia N',
+      contactInfo: '+123789456, opq@corporation.com',
+      mainContact: 'Robert Clark',
+      productsOrServices: 'Dịch vụ tư vấn',
+    },
+    {
+      companyName: 'STU Logistics',
+      address: '2223 Đường STU, Thành phố O, Quốc gia P',
+      contactInfo: '+456123789, stu@logistics.com',
+      mainContact: 'Jessica Martinez',
+      productsOrServices: 'Dịch vụ vận chuyển và logistics',
+    },
+  ];
+  const user = await prisma.user.findFirst();
   suppliers.map(async (supplier) => {
     const [phone, email] = supplier.contactInfo.split(',');
     await prisma.provider.create({
@@ -160,7 +172,21 @@ async function main() {
       },
     });
   });
+};
 
+const createCategories = async (prisma) => {
+  const categories = ['Cà phê', 'Trà', 'Sinh tố'];
+  categories.map(async (category) => {
+    return await prisma.category.create({
+      data: {
+        name: category,
+      },
+    });
+  });
+};
+
+const createMaterial = async (prisma) => {
+  const user = await prisma.user.findFirst();
   for (let i = 0; i < 10; i++) {
     await prisma.material.create({
       data: {
@@ -174,6 +200,50 @@ async function main() {
         unit_id: 1,
       },
     });
+  }
+};
+
+const createPayment = async (prisma) => {
+  const paymentType = ['Trả tiền mặt', 'Thanh toán qua thẻ ngân hàng'];
+
+  await prisma.payment.createMany({
+    data: paymentType.map((type) => {
+      return { type: type };
+    }),
+  });
+};
+
+const createDayStatistic = async (prisma) => {
+  const start = new Date('2024-01-01');
+  const end = new Date('2024-05-31');
+  let count = 0;
+  while (start !== end && count <= 1000) {
+    await prisma.statisticsByDay.create({
+      data: {
+        statistics_date: start,
+        number_of_order: randomPositiveInteger(1000),
+        revenue: randomPositiveInteger(10000000),
+      },
+    });
+    start.setDate(start.getDate() + 1);
+    count++;
+  }
+};
+
+async function main() {
+  try {
+    await prisma.$transaction(async (p) => {
+      await deleteTableRecord(p);
+      await createUser(p);
+      await createUnit(p);
+      await createProvider(p);
+      await createCategories(p);
+      await createMaterial(p);
+      await createPayment(p);
+      await createDayStatistic(p);
+    });
+  } catch (error) {
+    console.log(error);
   }
 }
 
